2a57e35d9e810b1296a994a03054a03feccfaf60
Paul Victor Meius <paulmeius@yahoo.com>
2022-02-05 13:07:21 +0200

    Second lambda

--- Lambdas.cpp
+++ Lambdas.cpp
@@ -3,5 +3,8 @@
 int main()
 {
 	[](const std::string& name) {std::cout << "Hello " << name << '\n'; }("Paul");
+	auto lambda1 = [](int x) { return x % 3 == 0; };
+	bool divByThree = lambda1(33);
+	std::cout << std::boolalpha << divByThree << '\n';
 }
 
